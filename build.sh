1128f45ad7417c611517dc791391b1bfe118c3c7
djpb <djpbadenhorst@gmail.com>
2023-01-05 11:15:14 +0200

    Clear up integration with upstream

--- build.sh
+++ build.sh
@@ -1,8 +1,36 @@
 rm -rf node_modules/
+rm -rf dist/
 rm -rf api/node_modules/
+rm -rf api/dist/
 rm -rf app/node_modules/
-rm pnpm-lock.yaml
+rm -rf app/dist/
+rm -rf packages/create-directus-extension/node_modules/
+rm -rf packages/create-directus-extension/dist/
+rm -rf packages/extensions-sdk/node_modules/
+rm -rf packages/extensions-sdk/dist/
+rm -rf packages/schema/node_modules/
+rm -rf packages/schema/dist/
+rm -rf packages/shared/node_modules/
+rm -rf packages/shared/dist/
+rm -rf packages/specs/node_modules/
+rm -rf packages/specs/dist/
+rm -rf packages/storage/node_modules/
+rm -rf packages/storage/dist/
+rm -rf packages/storage-driver-azure/node_modules/
+rm -rf packages/storage-driver-azure/dist/
+rm -rf packages/storage-driver-cloudinary/node_modules/
+rm -rf packages/storage-driver-cloudinary/dist/
+rm -rf packages/storage-driver-gcs/node_modules/
+rm -rf packages/storage-driver-gcs/dist/
+rm -rf packages/storage-driver-local/node_modules/
+rm -rf packages/storage-driver-local/dist/
+rm -rf packages/storage-driver-s3/node_modules/
+rm -rf packages/storage-driver-s3/dist/
+rm -rf packages/utils/node_modules/
+rm -rf packages/utils/dist/
+rm -rf $(pnpm store path)
+ 
 pnpm install
 NODE_OPTIONS="--max-old-space-size=8192" pnpm -r build
-node docker/pack.js
+node docker/pack
 docker build --no-cache -t directus-base -f ./docker/Dockerfile .
